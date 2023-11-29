323d064fddf77f583eaf31fc738378f1c99706ff
Sushant5050 <148970856+Sushant5050@users.noreply.github.com>
2023-11-29 15:14:50 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstproject
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a, b, c;
+            a = 10; b=20; c=a+b;
+            Console.WriteLine("Add=" + c);
+        }
+
+        public void Mul()
+        {
+            int a, b, c;
+            a = 10; b = 20; c = a * b;
+            Console.WriteLine("mul=" + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Add();
+            p.Mul();
+            Console.ReadLine();
+        }
+    }
+}
